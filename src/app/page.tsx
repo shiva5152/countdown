0535b5accf13d0362af3d53043ac611aa039abf9
shiva5152 <sg2304366@gmail.com>
2023-12-05 19:19:06 +0530

    robo fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className="min-h-screen _hero p-[20px] md:py-12  md:px-24 relative  overflow-x-hidden sm:overflow-y-hidden ">
+    <section className="h-screen _hero p-[20px] md:py-12  md:px-24 relative  overflow-x-hidden sm:overflow-y-hidden ">
       <Hero />
 
       <img
-        className="absolute sm:h-[1000px] sm:bottom-[-30%] sm:right-[0] bottom-[-20%] right-[-20%]"
-        src={"/image/robo.png"}
+        className="absolute max-h-[500px] sm:bottom-[0] sm:right-[5%] bottom-[-20%] right-[0]"
+        src={"/image/robo-1.png"}
         alt=""
       />
     </section>
